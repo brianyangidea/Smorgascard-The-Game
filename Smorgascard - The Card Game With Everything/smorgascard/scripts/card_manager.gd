2b1ba23038f59ff15f805650773d6851610f4196
brianyangidea <87741808+brianyangidea@users.noreply.github.com>
2026-06-08 23:15:05 -0400

    Added card dragging and hover effects

--- Smorgascard - The Card Game With Everything/smorgascard/scripts/card_manager.gd
+++ Smorgascard - The Card Game With Everything/smorgascard/scripts/card_manager.gd
@@ -4,6 +4,7 @@ const COLLISION_MASK_CARD = 1
 
 var card_being_dragged
 var screen_size
+var is_hovering_on_card
 
 
 func _ready() -> void:
@@ -23,9 +24,51 @@ func _input(event):
 		if event.pressed:
 			var card = check_for_card()
 			if card:
-				card_being_dragged = card
+				start_drag(card)
 		else:
-			card_being_dragged = null
+			finish_drag()
+			
+
+func start_drag(card):
+	card_being_dragged = card
+	card.scale = Vector2(0.8, 0.8)
+	
+	
+func finish_drag():
+	card_being_dragged.scale = Vector2(1.05, 1.05)
+	card_being_dragged = null
+	
+
+#Connects the card_hovvered_over and card_hovvered_off functions to each card
+func connect_card_signals(card):
+	card.connect("hovered", card_hovvered_over)
+	card.connect("hovered_off", card_hovvered_off)
+
+#Called when card is hovered over
+func card_hovvered_over(card):
+	if !is_hovering_on_card:
+		is_hovering_on_card = true
+		highlight_card(card, true)
+	
+#Called when card is hovered off
+func card_hovvered_off(card):
+	if !card_being_dragged:
+		highlight_card(card, false)
+		#Check if we hovered straight from one card to another
+		var new_card_hovered = check_for_card()
+		if new_card_hovered and new_card_hovered != card:
+			highlight_card(new_card_hovered, true)
+		else:
+			is_hovering_on_card = false
+	
+#Handles the highlighting of each card
+func highlight_card(card, hovered_status):
+	if hovered_status:
+		card.scale = Vector2(1.05, 1.05)
+		card.z_index = 2
+	else:
+		card.scale = Vector2(1, 1)
+		card.z_index = 1
 
 #This function checks where the mouse is clicking.
 #It returns the card being interacted with if it is over a card
@@ -38,5 +81,18 @@ func check_for_card():
 	parameters.collision_mask = COLLISION_MASK_CARD
 	var result = space_state.intersect_point(parameters)
 	if result.size() > 0:
-		return result[0].collider.get_parent()
+		return get_card_with_highest_z_index(result)
 	return null
+	
+func get_card_with_highest_z_index(cards):
+	#Assume first card has highest z index
+	var highest_z_card = cards[0].collider.get_parent()
+	var highest_z_index = highest_z_card.z_index
+	
+	#Loop through the rest of the cards to look for the highest z index (aka layer)
+	for i in range(1, cards.size()):
+		var current_card = cards[i].collider.get_parent()
+		if current_card.z_index > highest_z_index:
+			highest_z_card = current_card
+			highest_z_index = current_card.z_index
+	return highest_z_card
