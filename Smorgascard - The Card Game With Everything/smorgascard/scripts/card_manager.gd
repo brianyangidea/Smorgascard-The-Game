adc2f397088d7718a8e84db87601b8180ed4a2d3
brianyangidea <87741808+brianyangidea@users.noreply.github.com>
2026-06-08 23:52:44 -0400

    Added card slot function

--- Smorgascard - The Card Game With Everything/smorgascard/scripts/card_manager.gd
+++ Smorgascard - The Card Game With Everything/smorgascard/scripts/card_manager.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 const COLLISION_MASK_CARD = 1
+const COLLISION_MASK_CARD_SLOT = 2
 
 var card_being_dragged
 var screen_size
@@ -26,7 +27,8 @@ func _input(event):
 			if card:
 				start_drag(card)
 		else:
-			finish_drag()
+			if card_being_dragged:
+				finish_drag()
 			
 
 func start_drag(card):
@@ -36,6 +38,13 @@ func start_drag(card):
 	
 func finish_drag():
 	card_being_dragged.scale = Vector2(1.05, 1.05)
+	var card_slot_found = check_for_card_slot()
+	if card_slot_found and !card_slot_found.card_in_slot:
+		#Card dropped into slot
+		card_being_dragged.position = card_slot_found.position
+		#The two lines below prevents interaction with cards alrady in a slot
+		card_being_dragged.get_node("Area2D/CollisionShape2D").disabled = true
+		card_slot_found.card_in_slot = true
 	card_being_dragged = null
 	
 
@@ -82,6 +91,17 @@ func check_for_card():
 	var result = space_state.intersect_point(parameters)
 	if result.size() > 0:
 		return get_card_with_highest_z_index(result)
+	return null
+	
+func check_for_card_slot():
+	var space_state = get_viewport().world_2d.direct_space_state
+	var parameters = PhysicsPointQueryParameters2D.new()
+	parameters.position = get_viewport().get_mouse_position()
+	parameters.collide_with_areas = true
+	parameters.collision_mask = COLLISION_MASK_CARD_SLOT
+	var result = space_state.intersect_point(parameters)
+	if result.size() > 0:
+		return result[0].collider.get_parent()
 	return null
 	
 func get_card_with_highest_z_index(cards):
