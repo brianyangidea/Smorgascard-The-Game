3e29f9a5135022fa322f56d6fe2c17017ead3f0f
brianyangidea <87741808+brianyangidea@users.noreply.github.com>
2026-08-21 14:22:14 -0400

    Doing a hard reset on history due to technical reasons: - Old history far too messy - Outdated code - Mirror issues

--- Smorgascard - The Card Game With Everything/smorgascard/scripts/enemy_card.gd
+++
@@ -1,9 +0,0 @@
-
-extends Node2D
-
-var card_type
-var hand_position
-var card_slot_card_is_in
-
-var health
-var attack
--- Smorgascard - The Card Game With Everything/smorgascard/scripts/monster_card_slot.gd
+++
@@ -1,6 +0,0 @@
-extends Node2D
-
-#Is there a card in the slot???? Woahhhhhh!
-#Revolutionary. :3
-var card_in_slot = false
-var card_slot_type = "monster"
